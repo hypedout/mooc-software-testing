641ee8b45abdcbf39498813dc9e503ab723b1488
Niels <hypedout@gmail.com>
2019-10-19 16:49:07 +0200

    CeasarShift

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,21 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
 
+  private CaesarShiftCipher ceasar;
+
+  @BeforeEach
+  public void initialize() {
+    this.ceasar = new CaesarShiftCipher();
+  }
+
+  @Test // BUG 1
+  public void simple() {
+    String result = ceasar.CaesarShiftCipher("hal", 1);
+    Assertions.assertEquals("ibm", result);
+  }
 }
